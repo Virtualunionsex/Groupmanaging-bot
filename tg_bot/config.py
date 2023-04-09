a021b842a2e15a98ba96545edd3d7499db66825f
MalikGit <97935254+Virtualunionsex@users.noreply.github.com>
2023-04-09 12:10:41 +0700

    Update config.py

--- tg_bot/config.py
+++ tg_bot/config.py
@@ -1,12 +1,12 @@
 from tg_bot.sample_config import Config
 
 class Development(Config):
-    OWNER_ID = 1659580762  # my telegram ID
-    OWNER_USERNAME = "Gbotid"  # my telegram username
-    API_KEY = "5862381673:AAFcmBM8sz6l-Ojc7gADdFr6m_nxwwCTBNQ"  # my api key, as provided by the botfather
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:svLbmtihf65NIiz0B4UX@containers-us-west-155.railway.app:5476/railway'  # sample db credentials
-    MESSAGE_DUMP = '-1001839097484' # some group chat that your bot is a member of
+    OWNER_ID = 1715348447  # my telegram ID
+    OWNER_USERNAME = "cyellaku"  # my telegram username
+    API_KEY = "5940961389:AAFuoDsX0JpYKH4l_k8gCZc6yzg6uDsdRCc"  # my api key, as provided by the botfather
+    SQLALCHEMY_DATABASE_URI = 'postgresql://pfewsazv:NFJuP00vvb9dup3vS8jzp0ZpNYusWXtS@raja.db.elephantsql.com/pfewsazv'  # sample db credentials
+    MESSAGE_DUMP = '-1001729142523' # some group chat that your bot is a member of
     USE_MESSAGE_DUMP = True
-    SUDO_USERS = [18673980, 83489514]  # List of id's for users which have sudo access to the bot.
+    SUDO_USERS = [1715348447, 1908660708]  # List of id's for users which have sudo access to the bot.
     LOAD = []
     NO_LOAD = ['translation']
